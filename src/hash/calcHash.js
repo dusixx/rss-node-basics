cd4058a9c579c769fadfbd68e3e636c177795ab7
dusixx <113414935+dusixx@users.noreply.github.com>
2025-10-23 12:27:56 +0300

    feat: implement hash scripts

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,17 @@
+import { pathExists, resolvePath } from "../utils.js";
+import crypto from 'crypto';
+import stream from 'stream/promises';
+import fs from 'fs';
+
 const calculateHash = async () => {
-  // Write your code here
+  const src = resolvePath('hash/files/fileToCalculateHashFor.txt');
+  if (!await pathExists(src)) {
+    throw new FSOperationError();
+  }
+  const sha256 = crypto.createHash('sha256');
+  await stream.pipeline(fs.createReadStream(src), sha256);
+
+  console.log(sha256.digest('hex'));
 };
 
 await calculateHash();
