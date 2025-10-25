d3d1adb918dc152f3801db8d4a30d98b54056fb4
dusixx <113414935+dusixx@users.noreply.github.com>
2025-10-25 11:54:52 +0300

    feat: implement zip scripts

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,11 @@
+import { resolvePath, Log, gzipFile } from "../utils/index.js";
+
 const compress = async () => {
-  // Write your code here
+  const src = resolvePath('zip/files/fileToCompress.txt');
+  const dst = resolvePath('zip/files/archive.gz');
+
+  await gzipFile(src, dst);
+  Log.success('\nSuccessfully compressed!');
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,11 @@
+import { resolvePath, Log, gunzipFile } from "../utils/index.js";
+
 const decompress = async () => {
-  // Write your code here
+  const src = resolvePath('zip/files/archive.gz');
+  const dst = resolvePath('zip/files/fileToCompress.txt');
+
+  await gunzipFile(src, dst);
+  Log.success('\nSuccessfully decompressed!');
 };
 
 await decompress();
