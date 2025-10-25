e81003c57ca025632bf4dff104c1c08861b3e0a9
dusixx <113414935+dusixx@users.noreply.github.com>
2025-10-25 10:16:37 +0300

    refactor: update hash scripts

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,17 +1,23 @@
-import { pathExists, resolvePath } from "../utils.js";
+import { checkPath, FSOperationError, resolvePath } from "../utils/fs.js";
 import crypto from 'crypto';
 import stream from 'stream/promises';
 import fs from 'fs';
+import { Log } from "../utils/misc.js";
+
+const VALID_HASH = '7b90ad9e325c1c22b15c36cbe19413e3c471e5a711b8b828c8ebfcfd71d1d6db'
 
 const calculateHash = async () => {
   const src = resolvePath('hash/files/fileToCalculateHashFor.txt');
-  if (!await pathExists(src)) {
+
+  const { exists } = await checkPath(src);
+  if (!exists) {
     throw new FSOperationError();
   }
   const sha256 = crypto.createHash('sha256');
   await stream.pipeline(fs.createReadStream(src), sha256);
+  const hash = sha256.digest('hex');
 
-  console.log(sha256.digest('hex'));
+  Log[hash === VALID_HASH ? 'success' : 'error']('\nSHA256:', hash, '\n');
 };
 
 await calculateHash();
