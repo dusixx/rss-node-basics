5537f37b92ddc1e20cbf4e6d33de4f5ac720d633
dusixx <113414935+dusixx@users.noreply.github.com>
2025-10-26 21:57:37 +0200

    refactor: update hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -3,8 +3,6 @@ import fs from 'fs';
 import stream from 'stream/promises';
 import { checkPath, FSOperationError, Log, resolvePath } from "../utils/index.js";
 
-const VALID_HASH = '7b90ad9e325c1c22b15c36cbe19413e3c471e5a711b8b828c8ebfcfd71d1d6db'
-
 const calculateHash = async () => {
   const src = resolvePath('hash/files/fileToCalculateHashFor.txt');
 
@@ -16,9 +14,7 @@ const calculateHash = async () => {
   await stream.pipeline(fs.createReadStream(src), sha256);
   const hash = sha256.digest('hex');
 
-  const color = hash === VALID_HASH ? 'green' : 'red';
-  const mark = hash === VALID_HASH ? '✓' : '✗';
-  console.log(Log.style(color, `\n${mark} [SHA256]:`), hash);
+  console.log(Log.style("green", `\n✓ [SHA256]:`), hash);
 };
 
 await calculateHash();
