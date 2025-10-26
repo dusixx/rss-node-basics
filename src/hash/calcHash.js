5046c6a8750915510bf8d1fcc06b99b2f00ae1ee
dusixx <113414935+dusixx@users.noreply.github.com>
2025-10-26 12:06:58 +0200

    refactor: update hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,22 +1,24 @@
-import { checkPath, FSOperationError, resolvePath, Log } from "../utils/index.js";
 import crypto from 'crypto';
-import stream from 'stream/promises';
 import fs from 'fs';
+import stream from 'stream/promises';
+import { checkPath, FSOperationError, Log, resolvePath } from "../utils/index.js";
 
 const VALID_HASH = '7b90ad9e325c1c22b15c36cbe19413e3c471e5a711b8b828c8ebfcfd71d1d6db'
 
 const calculateHash = async () => {
   const src = resolvePath('hash/files/fileToCalculateHashFor.txt');
 
-  const { exists } = await checkPath(src);
-  if (!exists) {
+  const { exists, isFile } = await checkPath(src);
+  if (!exists || !isFile) {
     throw new FSOperationError();
   }
   const sha256 = crypto.createHash('sha256');
   await stream.pipeline(fs.createReadStream(src), sha256);
   const hash = sha256.digest('hex');
 
-  Log[hash === VALID_HASH ? 'success' : 'error']('\nSHA256:', hash, '\n');
+  const color = hash === VALID_HASH ? 'green' : 'red';
+  const mark = hash === VALID_HASH ? '✓' : '✗';
+  console.log(Log.style(color, `\n${mark} [SHA256]:`), hash);
 };
 
 await calculateHash();
