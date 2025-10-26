5f5bd623245bcfb0d3b4cc41c8a05feaddaa14a9
dusixx <113414935+dusixx@users.noreply.github.com>
2025-10-26 12:46:52 +0200

    refactor: update wt

--- src/wt/main.js
+++ src/wt/main.js
@@ -2,11 +2,11 @@ import { cpus } from 'os';
 import { checkPath, createWorker, FSOperationError, Log, resolvePath } from '../utils/index.js';
 
 const INIT_NUM = 10;
-const LOGICAL_CPU_CORES_LEN = cpus().length;
+const LOGICAL_CPU_CORES = cpus().length;
 
-const runFibCalcTasks = async (workerPath) => {
+const calculateFibonacci = async (workerPath) => {
   const workers = Array
-    .from({ length: LOGICAL_CPU_CORES_LEN })
+    .from({ length: LOGICAL_CPU_CORES })
     .map((_, id) => createWorker(workerPath, { id, data: INIT_NUM + id }));
 
   return (await Promise.all(workers)).map(({ status, data }) => ({ status, data }));
@@ -15,12 +15,12 @@ const runFibCalcTasks = async (workerPath) => {
 const performCalculations = async () => {
   const workerPath = resolvePath('wt/worker.js');
 
-  const { exists } = await checkPath(workerPath);
-  if (!exists) {
+  const { exists, isFile } = await checkPath(workerPath);
+  if (!exists || !isFile) {
     throw new FSOperationError();
   }
-  Log.info(`\nResults (${LOGICAL_CPU_CORES_LEN} logical CPU cores):`);
-  console.log(await runFibCalcTasks(workerPath));
+  Log.info(`\nResults (${LOGICAL_CPU_CORES} logical CPU cores):`);
+  console.log(await calculateFibonacci(workerPath));
 };
 
 await performCalculations();
